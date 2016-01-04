c010cd8c39042dbd7d99662488d640c0d213f434
Walfo <kesuiket.info@gmail.com>
2016-01-04 15:07:34 +0900

    Create once.js

---
+++ snippets/once.js
@@ -0,0 +1,25 @@
+
+/**
+ * once
+ * @param {Function} fn
+ * @param {Object} context
+ */
+function once(fn, context) {
+  var result;
+  
+  return function() {
+    if (fn) {
+      result = fn.apply(context || this, arguments);
+      fn = null;
+    }
+    return result;
+  };
+}
+
+
+// usage
+var canOnlyFireOnce = once(function() {
+  console.log('Fired!');
+});
+
+canOnlyFireOnce();
