81412dd81be5ca54a86456285accdaac542d049c
Walfo <kesuiket.info@gmail.com>
2016-01-04 14:54:35 +0900

    Update poll.js

--- snippets/poll.js
+++ snippets/poll.js
@@ -6,20 +6,25 @@
  */
 
 function poll(fn, callback, errback, timeout, interval) {
-  var endTime = (+new Date()) + (timeout || 2000);
-  interval = interval || 100;
+ var endTime = (+new Date()) + (timeout || 2000);
+ interval = interval || 100;
   
-  (function p() {
-    if (fn()) {
-      callback();
-    } else if ((+new Date() < endTime)) {
-      setTimeout(p, interval);
-    } else {
-      errback(new Error('timed out for ' + fn + ': ' + arguments));
-    }
-    
-  })();
+ (function p() {
+  if (fn()) {
+   callback();
+  } else if ((+new Date() < endTime)) {
+   setTimeout(p, interval); 
+  } else {
+   errback(new Error('timed out for ' + fn + ': ' + arguments));
+  }
+ })();
 }
 
 // usage
-poll(function() {)
+poll(function() {
+ return document.getElementById('lightbox').offsetWidth > 0;
+}, function() {
+ // Done, success callback
+}, function() {
+ // Error, failure callback
+});
