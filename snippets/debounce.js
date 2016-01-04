707dd8458698ade6c02a7f05b7b7e89a26d059c2
Walfo <kesuiket.info@gmail.com>
2016-01-04 14:31:53 +0900

    Create debounce.js

---
+++ snippets/debounce.js
@@ -0,0 +1,12 @@
+
+/**
+ * debounce
+ */
+function debounce(func, wait, immediate) {
+  var timeout;
+  return function() {
+    var context = this, args = arguments;
+    var later = function() {
+    
+  }
+}
