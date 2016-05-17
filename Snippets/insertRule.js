4fa8a76afec9812d6af0d5e43e0d51ee6098f261
Walfo <kesuiket.info@gmail.com>
2016-05-17 18:54:33 +0900

    Update insertRule.js

--- Snippets/insertRule.js
+++ Snippets/insertRule.js
@@ -6,9 +6,9 @@ var sheet = (function() {
   var style = document.createElement('style');
   
   style.appendChild(document.createTextNode(''));
-  document.head.appendChild('style');
+  document.head.appendChild(style);
   return style.sheet;
 })();
 
 // usage
-sheet.insertRule('header { float: left; opacity: 0.8; }', 1);
+sheet.insertRule('header { float: left; opacity: 0.8; }', 0);
