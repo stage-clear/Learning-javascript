7c77ad3a540dda3b7094935dbd8edb449ed518dd
Walfo <kesuiket.info@gmail.com>
2016-05-19 14:05:15 +0900

    Update injectCSS.js

--- Snippets/injectCSS.js
+++ Snippets/injectCSS.js
@@ -6,21 +6,34 @@
  */
 
 function injectCSS(rule, index) {
+  if (!rule) return ;
+  var head = document.head || document.getElementsByTagName('head')[0];
   var style = document.createElement('style');
   style.type = 'text/css';
   style.appendChild(document.createTextNode(''));
-  rule = rule.replace(/\}/g, '}\n');
-  
-  if (style.styleSheet) {
-    // for IE, cssText propurty in style object
-    style.styleSheet.cssText = rule;
-  } else {
-    // other modurn browsers
-    // style.appendChild(document.createTextNode(rule)); 
-    style.sheet.insertRule(rule, index || 0);
+  head.appendChild(style);
+
+  if (isArray(rule)) {
+    index = index || 0;
+
+    for (var i = 0; i < rule.length; i++) {
+      style.sheet.insertRule(rule[i], index);
+    }
+  } else if (isString(rule)) {
+    rule = rule.replace(/\}/g, '}\n');
+    if (style.styleSheet) {
+      // ie
+      style.stylesheet.cssText = rule;
+    } else {
+      // other modurn browsers
+      style.appendChild(document.createTextNode(rule));
+    }
   }
-  
+
   return style;
+
+  function isString(x) { return typeof x === 'string'; }
+  function isArray(x) { return typeof x === 'object' && x.slice; }
 }
 
 injectCSS('div { width: 100px; }');
