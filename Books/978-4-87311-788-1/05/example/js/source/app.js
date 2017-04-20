a1a1cbe944e3a2c3f0a4afa644fe4abaf17b05b3
kesuiket <ke.walfo@gmail.com>
2017-04-20 19:44:12 +0900

    Update app.js

--- Books/978-4-87311-788-1/05/example/js/source/app.js
+++ Books/978-4-87311-788-1/05/example/js/source/app.js
@@ -1,6 +1,12 @@
+'use strict'
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Logo from './components/Logo'
+
 ReactDOM.render(
   <h1>
-    <Logo /> アプリケーションにようこそ!
+    <Logo />
   </h1>,
   document.getElementById('app')
 )
