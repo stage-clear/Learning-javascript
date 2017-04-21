84adb9841fd6e92bf30fc1a5b309a940ff290704
kesuiket <ke.walfo@gmail.com>
2017-04-21 13:14:59 +0900

    Update deploy.sh

--- Books/978-4-87311-788-1/05/example/scripts/deploy.sh
+++ Books/978-4-87311-788-1/05/example/scripts/deploy.sh
@@ -1,1 +1,17 @@
+# 以前のバージョンをクリーンアップ
+rm -rf __deployme
+mkdir __deployme
 
+# ビルド
+sh scripts/build.sh
+
+# JavaScript のミニファイ
+uglify -s bundle.js -o __deployme/bundle.js
+# CSS のミニファイ
+cssshrink bundle.css > __deployme/bundle.css
+# HTMLと画像のコピー
+cp index.html __deployme/index.html
+cp -r images/ __deployme/images/
+
+# 完了
+date; echo;
