357194424690ee76d1d5bf36d4524f1f63b4e496
Keisuke Takeshima <ke.walfo@gmail.com>
2017-10-26 03:17:28 +0900

    Create script.js

---
+++ Libraries/amCharts/examples/toggling-export-annotation-mode/script.js
@@ -0,0 +1,56 @@
+
+var chart = AmCharts.makeChart('chartdiv', {
+  'type': 'serial',
+  'theme': 'light',
+  'dataProvider': [
+    {
+      'country': 'USA',
+      'visits: 2025,
+    },
+    {
+      'country': 'China',
+      'visits': 1882,
+    },
+    // ...
+  ],
+  'valueAxes': [{
+    'gridAlpha': 0.2,
+    'dashLength': 0,
+  }],
+  'gridAboveGraphs': true,
+  'graphs': [{
+    'balloonText': '[[category]]: <b>[[value]]</b>',
+    'fillAlphas': 0.8,
+    'lineAlpha': 0.2,
+    'type': 'column',
+    'valueField': 'visits',
+  }],
+  'chartCursor': {
+    'categoryBalloonEnabled': false,
+    'cursorAlpha': 0,
+    'zoomable': false,
+  },
+  'categoryField': 'country',
+  'categoryAxis': {
+    'gridPosition': 'start',
+    'gridAlpha': 0,
+    'tickPosition': 'start',
+    'tickLength': 20,
+  },
+  'export': {
+    'enabled': true,
+  },
+})
+
+function toggleAnnotations () {
+  if (chart['export'].drawing.enabled) {
+    chart['export'].drawing.handler.done()
+  } else {
+    chart['export'].capture({
+      'action': 'draw',
+    }, function() {
+      var drawingMenu = chart['export'].config.menu[0].menu[2].menu
+      chart['export'].createMenu(drawingMenu)
+    })
+  }
+}
