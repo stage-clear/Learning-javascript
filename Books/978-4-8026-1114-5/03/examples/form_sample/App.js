c24af19acecca7fea5e67e0c59695286661576ad
Keisuke Takeshima <ke.walfo@gmail.com>
2017-11-08 19:26:01 +0900

    Create App.js

---
+++ Books/978-4-8026-1114-5/03/examples/form_sample/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { SimpleForm } from './SimpleForm'
+import './App.css'
+
+// メイン画面のコンポーネント
+export default class App extends Component {
+  render () {
+    return (
+      <div className='App'>
+        <SimpleForm />
+      </div>
+    )
+  }
+}
