24af990425f46514f081d40de6be577e08118f73
Keisuke Takeshima <ke.walfo@gmail.com>
2017-11-09 16:36:26 +0900

    Update NumberForm.js

--- Books/978-4-8026-1114-5/03/examples/form_number/src/NumberForm.js
+++ Books/978-4-8026-1114-5/03/examples/form_number/src/NumberForm.js
@@ -1,1 +1,38 @@
 import React, { Component } from 'react'
+
+// 数字入力コンポーネント
+export default class NumberForm extends Component {
+  constructor (props) {
+    super(props)
+    this.state = { value: '' }
+  }
+  
+  // 値が変更された時
+  doChange (e) {
+    const curValue = e.target.value
+
+    // 数字以外は削除
+    const newValue = curValue.replace(/[^0-0]/g, '')
+    this.state({ value: newValue })
+  }
+  
+  // 送信ボタンが押された時
+  doSubmit (e) {
+    window.alert('値を送信: ' + this.state.value)
+    e.preventDefault()
+  }
+  
+  // 画面の描画
+  render () {
+    // イベントをメソッドにバインド
+    const doSubmit = (e) => this.doSubmit(e)
+    const doChange = (e) => this.doChange(e)
+    
+    return (
+      <form onSubmit={doSubmit}>
+        <input type='text' value={this.state.value} onChange={doChange}/>
+        <input type='submit' value='送信'/>
+      </form>
+    )
+  }
+}
