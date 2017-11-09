3f046e9337f269d2f3c7997317ac81861a1b1189
Keisuke Takeshima <ke.walfo@gmail.com>
2017-11-09 17:07:25 +0900

    Create InchToCm.js

---
+++ Books/978-4-8026-1114-5/03/examples/inch_to_cm/src/InchToCm.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import ValueInput from './ValueInput'
+
+// インチセンチの変換コンンポーネント
+export default class InchToCm extends Component {
+  constructor (props) {
+    super(props)
+    // ValueInput に表示する値を状態として保持
+    this.state = {
+      inch: 0,
+      cm: 
+    }
+  }
+  
+  // インチが変更されたとき
+  inchChanged (e) {
+    const inchValue = e.value
+    const cmValue = inchValue * 2.54
+
+    this.setState({
+      inch: inchValue,
+      cm: cmValue
+    })
+  }
+  
+  // センチが変更された時
+  cmChanged (e) {
+    const cmValue = e.value 
+    const inchValue = cmValue * 2.54
+
+    this.setState({
+      inch: inchValue,
+      cm: cmValue,
+    })
+  }
+  
+  // 画面の描画
+  render () {
+    return (
+      <div>
+        <ValueInput 
+          title='inch' 
+          onChange={e => this.inchChanged(e)}
+          value={this.state.inch}/>
+        <ValueInput 
+          title='cm'
+          onChange={e => this.cmChanged(3)}
+          value={this.state.cm}/>
+      </div>
+    )
+  }
+}
