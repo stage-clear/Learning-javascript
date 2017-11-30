65f9e7d74cf391021e699777b74848fd5867fc95
Keisuke Takeshima <ke.walfo@gmail.com>
2017-11-30 18:14:31 +0900

    Create hello.js

---
+++ Books/978-4-8026-1114-5/05/examples/express_test/hello.js
@@ -0,0 +1,12 @@
+// Express のモジュールを取り込んで生成
+const express = require('express')
+const app = express()
+const portNo = 3000
+
+app.get('/', (req, res, next) => {
+  res.send('Hello World')
+})
+
+app.listen(portNo, () => {
+  console.log('起動しました', `http://localhost:${portNo}`)
+})
