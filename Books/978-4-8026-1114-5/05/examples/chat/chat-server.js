280bec4863b780258a77106a67126fb2a41b9b52
Keisuke Takeshima <ke.walfo@gmail.com>
2017-12-22 20:30:01 +0900

    Create chat-server.js

---
+++ Books/978-4-8026-1114-5/05/examples/chat/chat-server.js
@@ -0,0 +1,28 @@
+// HTTPサーバーを作成（アプリを送信するため）
+const express = require('express')
+const app = express()
+const server = require('http').createServer(app)
+const portNo = 3001
+
+server.listen(portNo, () => {
+  console.log('起動しました', 'http://localhost:' + portNo)
+})
+
+// public ディレクトリのファイルを自動で返す
+app.use('/public', express.static('./public'))
+app.get('/', (req, res) => {
+  res.redirect(302, '/public')
+})
+
+// WebSocket サーバーを起動
+const socketio = require('socket.io')
+const io = socketio.listen(server)
+
+// クライアント
+io.on('connection', (socket) => {
+  console.log('ユーザーが接続', socket.client.id)
+  // メッセージ受信時の処理を記述
+  socket.on('chat-msg', (msg) => {
+    // 特定のメッセージを受信した時の処理をここに記述
+  })
+})
