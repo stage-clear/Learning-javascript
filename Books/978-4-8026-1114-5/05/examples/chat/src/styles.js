a7c062fb384b2d87ad9041c16c5ebd9264bbf8df
Keisuke Takeshima <ke.walfo@gmail.com>
2018-01-01 17:56:53 +0900

    Create styles.js

---
+++ Books/978-4-8026-1114-5/05/examples/chat/src/styles.js
@@ -0,0 +1,26 @@
+
+export default {
+  h1: {
+    backgroundColor: 'red',
+    color: 'white',
+    padding: 12
+  },
+  form: {
+    border: '1px solid gray',
+    padding: 12
+  },
+  log: {
+    borderBottom: '1px solid silver',
+    padding: 6,
+    margin: 6
+  },
+  name: {
+    float: 'left',
+    width: 100,
+    color: 'blue',
+    textAligh: 'right'
+  },
+  msg: {
+    float: 'left'
+  }
+}
