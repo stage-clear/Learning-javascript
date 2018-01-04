f1a4f03d2b8da56d77c6a71d8976079b41e4d219
Keisuke Takeshima <ke.walfo@gmail.com>
2018-01-04 18:54:11 +0900

    Create 6-server.js

---
+++ Books/978-4-8026-1114-5/06/examples/tegaki/6-server.js
@@ -0,0 +1,39 @@
+// 文字認識サーバー
+const path = require('path')
+const fs = require('fs')
+
+// 定数の定義
+const SVM_MODEL = path.join(__dirname, 'database', 'image-model.svm')
+const portNo = 3001
+
+// Webサーバーの起動
+const express = require('express')
+const app = express()
+app.listen(portNo, () => {
+  console.log('起動しました', `http://localhost:${portNo}`)
+})
+
+// 学習モデルの読み込み
+const svm = require('node-svm')
+const modelJSON = fs.readFileSync(SVM_MODEL, 'utf-8')
+const model = JSON.parse(modelJSON)
+const clf = svm.restore(model)
+
+// APIの定義
+app.get('/api/predict', (req, res) => {
+  const px = req.query.px
+  if (!px) {
+    res.json({status: false})
+    return 
+  }
+  
+  const pxa = px.split('').map(v => parseInt('0x', + v), 16)
+  console.log('受信:', pxa.join(''))
+  clf.predict(pxa).then((label) => {
+    res.json({status: true, label})
+    console.log('分類:', label)
+  })
+})
+
+// 静的ファイルの送出
+app.use('/', express.static('./public'))
