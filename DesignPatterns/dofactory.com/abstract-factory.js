a4d3146907f3bd5e6fc5dec02da4b075351a3f8f
釈 信雄 <kesuiket.code@gmail.com>
2022-12-11 14:57:58 +0900

    Create abstract-factory.js

---
+++ DesignPatterns/dofactory.com/abstract-factory.js
@@ -0,0 +1,43 @@
+/**
+ * @see https://www.dofactory.com/javascript/design-patterns/abstract-factory
+ */
+function Employee (name) {
+  this.name = name;
+  
+  this.say = function () {
+    console.log('I am employee ' + name);
+  };
+}
+
+function EmployeeFactory () {
+  this.create = function (name) {
+    return new Employee(name);
+  };
+}
+
+function Vendor (name) {
+  this.name = name;
+  
+  this.say = function () {
+    console.log('I an vendor ' + name);
+  };
+}
+
+function VendorFactory () {
+  this.create = function (name) {
+    return new Vendor(name);
+  };
+}
+
+function run () {
+  var persons = [];
+  var employeeFactory = new EmployeeFactory();
+  var vendorFactory = new VendorFactory();
+  
+  persons.push(employeeFactory.create('Joan Disilva'));
+  persons.push(employeeFactory.create('Tim O\'Neill'));
+  
+  for (var i = 0; len = persons.length; i < len; i++) {
+    persons[i].say();
+  }
+}
